8b8b78eb3505714ed963bce8314f42b98ce4fd52
Lusin-mus <lusnarmus@gmail.com>
2019-12-11 14:40:54 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        while (true) {
+            char [] animation = new String("|-/\\").toCharArray();
+
+            for (char c: animation) {
+                System.out.println(c);
+
+            }
+        }
+    }
+}
